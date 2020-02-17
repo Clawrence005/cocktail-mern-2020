24006cb20db829ffa2b1ad2a94922c7d4f3f8b8e
colin <clawrence005@yahoo.com>
2020-02-17 17:56:35 -0600

    playing with navigating the data obj

--- src/App.js
+++ src/App.js
@@ -14,17 +14,21 @@ class App extends React.Component {
           creatorName: 'unknown',
           isShaken: false,
           isDoubleStrain: true,
+          details: [
+            { info: '333' },
+            { info: '733' },
+            { info: '533' }
+          ],
 
-          recipe: {
-            ingredients: [
-              { ing: '2 oz london dry gin' },
-              { ing: '1 oz dry vermouth' },
-              { ing: '1 dash orange bitters' },
-            ],
-            garnish: 'lemon peel',
-            method: "stir the ingredients",
-            glass: "martini",
-          }
+          ingredients: [
+            { ing: '2 oz london dry gin' },
+            { ing: '1 oz dry vermouth' },
+            { ing: '1 dash orange bitters' },
+          ],
+          garnish: 'lemon peel',
+          method: "stir the ingredients",
+          glass: "martini",
+
         },
         {
           cocktailName: 'old fashioned',
@@ -34,16 +38,20 @@ class App extends React.Component {
           isShaken: false,
           isDoubleStrain: false,
 
-          recipe: {
-            ingredients: [
-              { ing: '2 oz rye whiskey' },
-              { ing: '.5 oz demerara syrup' },
-              { ing: '3 dash Angostura bitters' },
-            ],
-            garnish: 'lemon peel',
-            method: "stir the ingredients",
-            glass: "old fashioned glass",
-          }
+          details: [
+            { info: '333' },
+            { info: '733' },
+            { info: '533' }
+          ],
+
+          ingredients: [
+            { ing: '2 oz rye whiskey' },
+            { ing: '.5 oz demerara syrup' },
+            { ing: '3 dash Angostura bitters' },
+          ],
+          garnish: 'lemon peel',
+          method: "stir the ingredients",
+          glass: "old fashioned glass",
         },
         {
           cocktailName: 'mojito',
@@ -53,37 +61,46 @@ class App extends React.Component {
           isShaken: false,
           isDoubleStrain: false,
 
-          recipe: {
-            ingredients: [
-              { ing: '1.5 oz light cuban style rum' },
-              { ing: '.75 oz simple syrup' },
-              { ing: '.75 oz lime juice' },
-              { ing: '1 oz sparkling water' }
-            ],
-            garnish: 'muddle mint, add rest of ingredients, and top with sparkling water then ice',
-            method: "muddle in glass",
-            glass: "collins glass",
-          }
+          details: [
+            { info: '333' },
+            { info: '733' },
+            { info: '533' }
+          ],
+          ingredients: [
+            { ing: '1.5 oz light cuban style rum' },
+            { ing: '.75 oz simple syrup' },
+            { ing: '.75 oz lime juice' },
+            { ing: '1 oz sparkling water' }
+          ],
+          garnish: 'muddle mint, add rest of ingredients, and top with sparkling water then ice',
+          method: "muddle in glass",
+          glass: "collins glass",
         },
         {
           cocktailName: 'daiquiri',
           cocktailId: 4,
-          isClassic: true,
+          isClassic: false,
           creatorName: 'unknown',
           isShaken: true,
           isDoubleStrain: true,
 
-          recipe: {
-            ingredients: [
-              { ing: '2 oz light rum' },
-              { ing: '.75 oz simple syrup' },
-              { ing: '.75 oz lime juice' },
-              { ing: '1 oz sparkling water' }
-            ],
-            garnish: 'lime wheel',
-            method: "shake with ice and double strain",
-            glass: "coupe",
-          }
+          details: [
+            { info: '333' },
+            { info: '733' },
+            { info: '533' }
+          ],
+
+          ingredients: [
+            { ing: '2 oz light rum' },
+            { ing: '.75 oz simple syrup' },
+            { ing: '.75 oz lime juice' },
+            { ing: '1 oz sparkling water' }
+          ],
+
+          garnish: 'lime wheel',
+          method: "shake with ice and double strain",
+          glass: "coupe",
+
         },
         {
           cocktailName: 'new fashioned',
@@ -93,17 +110,22 @@ class App extends React.Component {
           isShaken: true,
           isDoubleStrain: true,
 
-          recipe: {
-            ingredients: [
-              { ing: '2 oz artisan gasoline' },
-              { ing: '.75 oz simple syrup' },
-              { ing: '.75 oz pop rocks' },
-              { ing: '1 oz sparkling water' }
-            ],
-            garnish: 'lime wheel',
-            method: "shake with ice and double strain",
-            glass: "coupe",
-          }
+          details: [
+            { info: '333' },
+            { info: '733' },
+            { info: '533' }
+          ],
+
+          ingredients: [
+            { ing: '2 oz light rum' },
+            { ing: '.75 oz simple syrup' },
+            { ing: '.75 oz lime juice' },
+            { ing: '1 oz sparkling water' }
+          ],
+
+          garnish: 'lime wheel',
+          method: "shake with ice and double strain",
+          glass: "coupe",
         },
 
       ]
@@ -113,13 +135,36 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        {/* {items.map((item, index) => {
+   return <div key={index}>
+{item.name}</div>; */}
+
         <ClassicCocktail
-          id={this.state.cocktails.cocktailId}
+          key={this.state.cocktails.cocktailId}
           list={this.state.cocktails.filter(cocktail => cocktail.isClassic === true)}
         />
+        <ClassicCocktail
+          key={this.state.cocktails.cocktailId}
+          list={this.state.cocktails.filter(cocktail => cocktail.isClassic !== true)}
+        />
+
+        {this.state.cocktails.map((cocktail) =>
+          <div>
+            <h3>{cocktail.cocktailName}</h3>
+            <ul>
+              {cocktail.ingredients.map((subItem) =>
+                <li>{subItem.ing}</li>
+              )}
+            </ul>
+          </div>
+        )}
+
       </div>
+
     )
   }
+
 }
+
 
 export default App;
--- src/components/ClassicCocktail/index.js
+++ src/components/ClassicCocktail/index.js
@@ -3,17 +3,16 @@ import React from 'react';
 function ClassicCocktail(props) {
   return (
     <div>
-
       <h4>Classic Cocktail</h4>
-      {props.list.map((cocktail) => (
-        <li key={cocktail.cocktailName}>
-          <span>{cocktail.cocktailName}</span>
-        </li>
-      ))}
 
-      {/* {items.map((item, index) => {
-   return <div key={index}>
-{item.name}</div>; */}
+      {/* {props.list.map((cocktail) => (
+        <div key={cocktail.cocktailId}>
+          cocktailName: <span>{cocktail.cocktailName}</span>
+          <li>{cocktail.cocktailId}</li>
+          <li>{cocktail.ingredients}</li>
+        </div> 
+
+      ))}*/}
 
     </div>
   )

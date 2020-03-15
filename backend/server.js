e8e1bce7fb206891fcba38deded3922e9f657509
colin <clawrence005@yahoo.com>
2020-03-15 02:41:45 -0500

    beginning to add user routes

--- backend/server.js
+++ backend/server.js
@@ -13,15 +13,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://127.0.0.1:27017/cocktailmern', { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
 
-
-// mongoose.connect(process.env.test_db, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
-
 const connection = mongoose.connection;
 connection.once('open', function () {
   console.log("MongoDB database connection established successfully");
 });
 const cocktailRoutes = express.Router();
 app.use('/cocktails', cocktailRoutes);
+
 
 
 cocktailRoutes.route('/').get(function (req, res) {
@@ -34,8 +32,7 @@ cocktailRoutes.route('/').get(function (req, res) {
   });
 });
 
-
-app.post('/cocktails/create', (req, res) => {
+cocktailRoutes.route('/create').post(function (req, res) {
   console.log('Got body:', req.body);
   let cocktail = new Cocktail({
     cocktailName: req.body.cocktailName,
@@ -50,7 +47,6 @@ app.post('/cocktails/create', (req, res) => {
     garnish: req.body.garnish,
     method: req.body.method,
     glass: req.body.glass
-
   });
   cocktail.save(function (err) {
     if (err) {
@@ -60,57 +56,6 @@ app.post('/cocktails/create', (req, res) => {
     res.json(cocktail)
   })
 });
-// cocktailRoutes.route('/create').post(function (req, res) {
-//   let cocktail = new Cocktail({
-//     cocktailName: req.body.cocktailName,
-//     glass: req.body.glass,
-//   });
-//   this.cocktail.save(function (err) {
-//     if (err) {
-//       return (err);
-//     }
-//     res.json(cocktail);
-//   })
-// });
-//   
-//   cocktail.save(function (err) {
-//     if (err) { return (err) }
-
-//     res.json({ cocktail });
-//   });
-// });
-
-// app.post('/cocktails/create', function (req, res) {
-//   req.collection.insert(req.body, {}, function (e, results) {
-//     if (e) return
-//     res.send(results)
-//   })
-// })
-
-// cocktailRoutes.route('/create').post(function (req, res, next) {
-//   console.log(req.body.cocktail);
-//   let cocktail = new Cocktail();
-
-//   cocktail.cocktailName = req.body.cocktailName;
-//   // cocktail.cocktailId = req.body.cocktailId;
-//   // cocktail.isClassic = req.body.isClassic;
-//   // cocktail.creatorName = req.body.creatorName;
-//   // cocktail.isShaken = req.body.isShaken;
-//   // cocktail.isDoubleStrain = req.body.isDoubleStrain;
-//   // details= req.body.details,
-//   // ingredients= req.body.ingredients,
-//   // cocktail.garnish = req.body.garnish;
-//   // cocktail.method = req.body.method;
-//   // cocktail.glass = req.body.glass;
-
-
-//   cocktail.save(function (err) {
-//     if (err) { return next(err) }
-
-//     res.json({ cocktail });
-//   });
-// });
-
 
 cocktailRoutes.route('/all').get(function (req, res) {
   Cocktail.find({}, function (err, cocktails) {
@@ -129,23 +74,24 @@ cocktailRoutes.route('/:id').get(function (req, res) {
   });
 });
 
-
-cocktailRoutes.route('/update-id/:id').post(function (req, res) {
+//have to update all fields right now
+cocktailRoutes.route('/update/:id').post(function (req, res) {
   Cocktail.findById(req.params.id, function (err, cocktail) {
     if (!cocktail)
       res.status(404).send('cocktail with this id not found');
     else
       cocktail.cocktailName = req.body.cocktailName;
-    cocktail.cocktailId = req.body.cocktailId;
+    cocktail.isClassic = req.body.isClassic;
     cocktail.creatorName = req.body.creatorName;
     cocktail.isShaken = req.body.isShaken;
+    cocktail.details = req.body.details;
     cocktail.ingredients = req.body.ingredients;
     cocktail.garnish = req.body.garnish;
     cocktail.method = req.body.method;
     cocktail.glass = req.body.glass;
 
     cocktail.save().then(cocktail => {
-      res.json(`cocktail ${cocktail.cocktailName} updated`)
+      res.status(200).json(`cocktail ${cocktail.cocktailName} updated`)
     })
 
 
@@ -160,6 +106,13 @@ cocktailRoutes.route('/delete/:id').delete(function (req, res) {
       res.status(200).send('cocktail with this id deleted');
   })
 });
+
+
+const userRoutes = express.Router();
+app.use('users', userRoutes);
+
+
+
 
 app.listen(PORT, function () {
   console.log("Server is running on Port: " + PORT);

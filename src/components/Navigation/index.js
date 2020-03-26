af850a2e8460f99ed897ceb8bf1ed2cf5f923baa
colin <clawrence005@yahoo.com>
2020-03-26 04:27:46 -0500

    created dummy nav

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navigation {
+  return (
+    <div className="nav">
+      <p>gghjhjhgh</p>
+      <p>dsad</p>
+      <p>gghadasdjhjadsadhgh</p>
+    </div>
+  )
+}
+
+export default Navigation;

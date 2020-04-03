86735373808e6eb25d66524019d44b52fef69f55
colin <clawrence005@yahoo.com>
2020-04-03 03:05:37 -0500

    ?

--- server.js
+++ server.js
@@ -18,13 +18,12 @@ let User = require('./model/user.model')
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("build"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 // ... other app.use middleware 
 // app.use(express.static(path.join(__dirname, "client", "build")))
-app.use('/', express.static(path.join(__dirname, '/client/build')));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
